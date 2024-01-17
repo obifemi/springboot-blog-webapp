563266061ac39efdd4ed45bb70c48246b7d693d5
a.inal <a.inal@mexxon.com>
2024-01-17 12:33:59 +0100

    add Post entity

---
+++ src/main/java/net/javaguides/springboot/entity/Post.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="posts", schema = "myblogapp")
+
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    private String url;
+    @Column(nullable = false)
+    private String content;
+    private String shortDescription;
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+
+}
